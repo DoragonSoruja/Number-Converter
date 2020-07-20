61c0845acc160509212304184c29af23974ee7ed
Sebastian Roberts <tiansebas09@gmail.com>
2020-07-20 18:02:54 -0500

    Starting

--- Number Converter/Form1.Designer.cs
+++ Number Converter/Form1.Designer.cs
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.userInput = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.results = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a number:";
+            // 
+            // userInput
+            // 
+            this.userInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userInput.Location = new System.Drawing.Point(159, 55);
+            this.userInput.Name = "userInput";
+            this.userInput.Size = new System.Drawing.Size(190, 21);
+            this.userInput.TabIndex = 1;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(355, 55);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 21);
+            this.convertButton.TabIndex = 2;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // results
+            // 
+            this.results.AutoSize = true;
+            this.results.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.results.Location = new System.Drawing.Point(154, 156);
+            this.results.Name = "results";
+            this.results.Size = new System.Drawing.Size(0, 24);
+            this.results.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Your number is:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(522, 273);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.results);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.userInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userInput;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Label results;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Number Converter/Form1.cs
+++ Number Converter/Form1.cs
@@ -16,5 +16,19 @@ namespace Number_Converter
         {
             InitializeComponent();
         }
+
+        string wordNumber = "";
+
+        string[] oneToNineTeen = new string[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+        string[] ten = new string[] { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+        string[] tens = new string[] { "", "", "Twenty ", "Thirty ", "Forty ", "Fifty ", "Sixty ", "Seventy ", "Eighty ", "Ninty " };
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            wordNumber = tens[Int64.Parse(userInput.Text) % 100 / 10] + oneToNineTeen[Int64.Parse(userInput.Text) % 10];
+            results.Text = wordNumber;
+        }
+
+        
     }
 }
