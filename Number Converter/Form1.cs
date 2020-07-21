bbb869378be3317a6188942b758431eb478b4d95
Sebastian Roberts <tiansebas09@gmail.com>
2020-07-21 17:40:19 -0500

    Test

--- Number Converter/Form1.cs
+++ Number Converter/Form1.cs
@@ -25,7 +25,46 @@ namespace Number_Converter
 
         private void convertButton_Click(object sender, EventArgs e)
         {
-            wordNumber = tens[Int64.Parse(userInput.Text) % 100 / 10] + oneToNineTeen[Int64.Parse(userInput.Text) % 10];
+            if (Int64.Parse(userInput.Text) < 20 && Int64.Parse(userInput.Text) > 9)
+            {
+                switch (Int64.Parse(userInput.Text))
+                {
+                    case 10:
+                        wordNumber = "Ten";
+                        break;
+                    case 11:
+                        wordNumber = "Eleven";
+                        break;
+                    case 12:
+                        wordNumber = "Twelve";
+                        break;
+                    case 13:
+                        wordNumber = "Thirteen";
+                        break;
+                    case 14:
+                        wordNumber = "Fourteen";
+                        break;
+                    case 15:
+                        wordNumber = "Ten";
+                        break;
+                    case 16:
+                        wordNumber = "Ten";
+                        break;
+                    case 17:
+                        wordNumber = "Ten";
+                        break;
+                    case 18:
+                        wordNumber = "Ten";
+                        break;
+                    case 19:
+                        wordNumber = "Ten";
+                        break;
+                }
+            }
+            else
+            {
+                wordNumber = tens[Int64.Parse(userInput.Text) % 100 / 10] + oneToNineTeen[Int64.Parse(userInput.Text) % 10];
+            }
             results.Text = wordNumber;
         }
 
