ca63e743c4b65403d971bafde1a835475cd64aa2
Sebastian Roberts <tiansebas09@gmail.com>
2020-07-25 10:11:48 -0500

    Mistake Fix

--- Number Converter/Form1.cs
+++ Number Converter/Form1.cs
@@ -205,8 +205,6 @@ namespace Number_Converter
 
             if (number > 0)
             {
-                if (words != "")
-                    words += "and ";
 
                 var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
                 var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
